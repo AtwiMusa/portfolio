e6288b208b9bfaa2122b96bc088dc079db0ac1a3
Musa Atwi <108957652+AtwiMusa@users.noreply.github.com>
2026-05-31 23:18:09 +0200

    Update script.js
    small changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-document.querySelectorAll('.poem-card').forEach(card => {
-  card.addEventListener('click', () => {
+document.querySelectorAll('.poem-card-wrapper').forEach(wrapper => {
+  wrapper.addEventListener('click', () => {
+    const card = wrapper.querySelector('.poem-card');
     card.classList.toggle('expanded');
   });
 });
